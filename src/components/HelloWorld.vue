d26d304e573957004ca9a6108e77dd01fab76f4b
HDU_Nbsp <1808347356@qq.com>
2022-07-31 23:56:39 +0800

    测试了一下基础组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
+  <div class="body">
     <img src="../assets/pic.png" style="width:800px">
+    <div class="text">
+      <h1>Hello World</h1>
+      <p>This is a simple Vue.js component.</p>
+      <p>{{xz}}</p>
+    </div>
   </div>
 </template>
 
@@ -9,10 +14,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      xz: '嗨嗨嗨，我是徐壮'
     }
   }
 }
 </script>
 <style scoped>
-
 </style>
