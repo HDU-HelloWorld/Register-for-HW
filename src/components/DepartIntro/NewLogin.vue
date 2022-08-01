657dcbe3dbdc03359f9d32869b54c2d72ae2acc6
travelless <2022742378@qq.com>
2022-08-02 00:23:25 +0800

    from-1.0 大概的框架与内容

---
+++ src/components/DepartIntro/NewLogin.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="login">
+    <div class="wrapper">
+      <el-form :model="form" :rules="rules" ref="form" label-width="80px">
+        <el-row>
+          <el-col :span="12" :offset="0">
+            <el-form-item label="姓名" prop="name" id="m">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12" :offset="0">
+            <el-form-item label="学号" prop="stuNum">
+              <el-input v-model="form.stuNum"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item label="所在学院" prop="college">
+          <el-input v-model="form.college"></el-input>
+        </el-form-item>
+        <el-form-item label="志愿部门">
+          <el-select v-model="form.department">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.label"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone">
+          <el-input v-model="form.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="qq号" prop="qqnumber">
+          <el-input v-model="form.qqnumber"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="submitForm('form')"
+            >提交</el-button
+          >
+          <el-button type="secondary" @click="resetForm('form')"
+            >清空</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewLogin',
+  data () {
+    return {
+      form: {
+        name: '',
+        stuNum: '',
+        phone: '',
+        qqnumber: '',
+        college: '',
+        department: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' },
+          { min: 0, max: 15, message: '长度在 0 到 15 个字符', trigger: 'blur' }
+        ],
+        stuNum: [
+          { required: true, message: '请输入学号', trigger: 'blur' },
+          { pattern: /^[0-9]*$/, message: '学号应为数字', trigger: 'blur' },
+          { min: 8, max: 8, message: '学号格式不正确', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: '请输入电话', trigger: 'blur' },
+          { pattern: /^1[3-9][0-9]\d{8}$/, message: '请输入正确的电话号码', trigger: 'blur' },
+          { min: 0, max: 15, message: '长度在 0 到 15 个字符', trigger: 'blur' }
+        ],
+        qqnumber: [
+          { required: true, message: '请输入qq号', trigger: 'blur' },
+          { pattern: /^[0-9]*$/, message: 'qq号应为数字', trigger: 'blur' },
+          { min: 5, max: 10, message: 'qq号格式不正确', trigger: 'blur' }
+        ],
+        college: [
+          { required: true, message: '请输入学院', trigger: 'blur' },
+          { pattern: /^[\u0391-\uFFE5A-Za-z]+$/, message: '请输入学院', trigger: 'blur' },
+          { min: 0, max: 15, message: '长度在 0 到 15 个字符', trigger: 'blur' }
+        ]
+      },
+      options: [{
+        value: '选项1',
+        label: '人工智能部门'
+      }, {
+        value: '选项2',
+        label: '行政部门'
+      }, {
+        value: '选项3',
+        label: '前端部门'
+      }, {
+        value: '选项4',
+        label: '后端部门'
+      }]
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+          console.log(this.form)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,17 +5,19 @@
         <el-tab-pane label="人工智能部门" name="first"><peoplesmart></peoplesmart></el-tab-pane>
         <el-tab-pane label="行政部门" name="second"><admin></admin></el-tab-pane>
         <el-tab-pane label="后端部门" name="third"><back></back></el-tab-pane>
+         <el-tab-pane label="报名表单" name="fouth"><NewLogin></NewLogin></el-tab-pane>
       </el-tabs>
     </div>
   </div>
 </template>
 
 <script>
-import Admin from './DepartIntro/Admin.vue'
-import Back from './DepartIntro/back.vue'
-import Peoplesmart from './DepartIntro/peoplesmart.vue'
+import Admin from './DepartIntro/Admin'
+import Back from './DepartIntro/Back'
+import Peoplesmart from './DepartIntro/Peoplesmart'
+import NewLogin from './DepartIntro/NewLogin'
 export default {
-  components: { Admin, Back, Peoplesmart },
+  components: { Admin, Back, Peoplesmart, NewLogin },
   name: 'HelloWorld',
   data () {
     return {
