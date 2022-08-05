e8c8e8ddb69eae88cf89caa8b7ad70366d7585c9
HDU_Nbsp <1808347356@qq.com>
2022-08-06 00:19:16 +0800

    添加了点击变换边框颜色的效果

--- src/components/DepIntro.vue
+++ src/components/DepIntro.vue
@@ -3,10 +3,26 @@
     <div class="contro">
       <!-- 部门介绍导航栏 -->
       <div class="imgs-icon">
-        <li @click="changeColor"><router-link  class="navlist" to="/"><img src="@/assets/front.png"></router-link></li>
-        <li @click="changeColor"><router-link  class="navlist" to="/Back"><img src="@/assets/back.jpg"></router-link></li>
-        <li @click="changeColor"><router-link  class="navlist" to="/Peoplesmart"><img src="@/assets/peoplesmart.png"></router-link></li>
-        <li @click="changeColor"><router-link  class="navlist" to="/Admin"><img src="@/assets/admin.png"></router-link></li>
+        <li @click="changeColor">
+          <router-link class="navList" ref="navlist" to="/"
+            ><img src="@/assets/front.png"
+          /></router-link>
+        </li>
+        <li @click="changeColor">
+          <router-link class="navList" ref="navlist" to="/Back"
+            ><img src="@/assets/back.jpg"
+          /></router-link>
+        </li>
+        <li @click="changeColor">
+          <router-link class="navList" ref="navlist" to="/Peoplesmart"
+            ><img src="@/assets/peoplesmart.png"
+          /></router-link>
+        </li>
+        <li @click="changeColor">
+          <router-link class="navList" ref="navlist" to="/Admin"
+            ><img src="@/assets/admin.png"
+          /></router-link>
+        </li>
       </div>
     </div>
     <div class="showinfo">
@@ -23,71 +39,66 @@ export default {
     }
   },
   methods: {
-    changeColor () {
-      let navlists = this.$refs.navList
-      console.log(navlists)
-      for (let i = 0; i < navlists.length; i++) {
-        navlists[i].onclick = function () {
-          // 恢复成默认样式
-          for (let j = 0; j < navlists.length; j++) {
-            navlists[j].style.outlineColor = '#fff'
-          }
-          console.log(this)
-          // 添加点击样式
-          this.style.outlineColor = 'rgba(117, 159, 231, 0.98)'
-        }
-      }
+    changeColor (index) {
+      console.log(index.target)
+      // 给target的父级元素添加class
+      index.target.parentNode.classList.add('borderBlue')
     }
   }
 }
 </script>
 <style scoped lang="less">
-  .content{
-    width: 100%;
-    position: absolute;
-    .contro{
-      position: relative;
+.content {
+  width: 100%;
+  position: absolute;
+  .contro {
+    position: relative;
+    z-index: 999;
+    width: 55%;
+    height: 80px;
+    top: 450px;
+    .imgs-icon {
+      margin-left: 40%;
       z-index: 999;
-      width:55%;
       height: 80px;
-      top: 450px;
-      .imgs-icon{
-        margin-left: 40%;
-        z-index: 999;
-        height: 80px;
-        width:60%;
-        display: flex;
-        justify-content: space-between;
-        align-content: center;
-        text-align: center;
-        li{
-          width: 15%;
+      width: 60%;
+      display: flex;
+      justify-content: space-between;
+      align-content: center;
+      text-align: center;
+      li {
+        width: 15%;
+        border-radius: 8px;
+        .navList {
+          display: block;
+          width: 100%;
           border-radius: 8px;
-          a{
-            display: block;
-            width: 100%;
+          transition: 0.2s;
+          outline: 2.8px solid #fff;
+          &:hover {
+            outline: 2.8px solid rgba(117, 159, 231, 0.98);
             border-radius: 8px;
-            transition: 0.2s;
-            outline: 2.8px solid #fff;
-            &:hover{
-              outline: 2.8px solid rgba(117, 159, 231, 0.98);
-              border-radius: 8px;
-            }
-            img{
-              box-sizing: border-box;
-              padding: 5px;
-              vertical-align: top;
-              border-radius: 8px;
-              width: 100%;
-            }
           }
+          img {
+            box-sizing: border-box;
+            padding: 5px;
+            vertical-align: top;
+            border-radius: 8px;
+            width: 100%;
+          }
+        }
+        // 将class同时有navList和borderBlue的outline颜色设为蓝色
+        .navList.borderBlue {
+          outline: 2.8px solid rgba(117, 159, 231, 0.98);
+          border-radius: 8px;
         }
       }
     }
-    .showinfo{
-      top: -80px;
-      width: 100%;
-      position: relative;
-    }
   }
+  .showinfo {
+    top: -80px;
+    width: 100%;
+    position: relative;
+  }
+}
 </style>
