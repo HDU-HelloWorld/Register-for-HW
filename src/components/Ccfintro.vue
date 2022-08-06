7f5f1e37e43d116aef86a592fcd6cfc96e77ebb2
lay <1617263137@qq.com>
2022-08-06 20:27:17 +0800

    ccf界面发展历程

--- src/components/Ccfintro.vue
+++ src/components/Ccfintro.vue
@@ -9,8 +9,9 @@
     </div>
     <div>
       <div class="title" id="1">简介</div>
-      <div class="text">
-        <p id="a">
+      <br>
+      <div class="">
+        <p>
           中国计算机学会（CCF）成立于1962年，全国一级学会，独立社团法人，中国科学技术协会成员。
         </p>
         <p>
@@ -18,11 +19,47 @@
         </p>
       </div>
     </div>
-    <br><br><br><br>
-    <div>
-      <div class="title" id="2">发展历程</div>
-      <div class="text">
-      </div>
+    <br /><br /><br /><br />
+    <div class="title" id="2">发展历程</div>
+    <br><br>
+    <div class="day12">
+      <el-timeline>
+        <el-timeline-item timestamp="1962年6月" placement="top" size="large" color="#65c5f1" class="left1">
+          <div class="card">
+            <h4>成立中国电子学会计算机专业委员会第一届委员会</h4>
+          </div>
+        </el-timeline-item>
+        <el-timeline-item timestamp="1985年3月" placement="top" color="#65c5f1" class="right">
+          <div class="card">
+            <h4>中国科协转发国家体改委批准成立中国计算机学会（全国一级学会）</h4>
+          </div>
+        </el-timeline-item>
+        <el-timeline-item timestamp="1989年8月" placement="top" color="#65c5f1" class="left2">
+          <div class="card">
+            <h4>学会主办的第一届国际青年计算机会议（ICYCS'89)在北京举行</h4>
+          </div>
+        </el-timeline-item>
+        <el-timeline-item timestamp="1998年5月" placement="top" color="#65c5f1" class="right">
+          <div class="card">
+            <h4>中国计算机学会青年计算机科技论坛YOCSEF创建</h4>
+          </div>
+        </el-timeline-item>
+        <el-timeline-item timestamp="1998年8月" placement="top" color="#65c5f1" class="left3">
+          <div class="card">
+            <h4>学会网站正式开通，<a href="http://www.ccf.org.cn">http://www.ccf.org.cn</a></h4>
+          </div>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2003年11月" placement="top" color="#65c5f1" class="right">
+          <div class="card">
+            <h4>第一届中国计算机大会（CNCC)在北京举行</h4>
+          </div>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2013年5月20日" placement="top" color="#65c5f1" class="left3">
+          <div class="card">
+            <h4>CCF早期成立的学生分会之一CCF杭州电子科技大学分会成立</h4>
+          </div>
+        </el-timeline-item>
+      </el-timeline>
     </div>
   </div>
 </template>
@@ -33,14 +70,12 @@ export default {
   data () {
     return {
     }
-  },
-  methods: {
   }
 }
 </script>
 
 <style scoped>
-.showinfo{
+.showinfo {
   margin-top: 600px;
 }
 .first {
@@ -95,9 +130,26 @@ p {
   border-right: 1px white solid;
 }
 .nav ul li a:hover {
-  background-color: rgb(163, 198, 238);
+  background-color: #65c5f1;
 }
 .nav ul li a.last {
   border-right: none;
+}
+.history {
+  margin-left: 45%;
+}
+.card{
+  margin:5px 0;
+}
+.el-timeline {
+  width: 80%;
+  position: relative;
+  left: 30%;
+  font-size: 20px;
+}
+.day12 {
+  width: 100%;
+  display: inline-block;
+  margin-right: 10%;
 }
 </style>
