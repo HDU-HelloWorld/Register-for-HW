b3954b6db2bf62d33668001ec924616e15905954
HDU_Nbsp <1808347356@qq.com>
2022-09-15 19:01:26 +0800

    完成了移动端报名表的开发

--- src/pages/MobilePage.vue
+++ src/pages/MobilePage.vue
@@ -56,7 +56,13 @@
               class="input auth-code"
               v-model="authCode"
             ></el-input>
-            <button class="get-authcode">获取验证码</button>
+            <button
+              class="get-authcode"
+              @click.prevent="getAuthCode"
+              :disabled="!authCodeButton"
+            >
+              {{ authCodeButtonText }}
+            </button>
           </span>
         </el-form>
         <el-form>
@@ -110,7 +116,10 @@ export default {
   name: 'MobilePage',
   data () {
     return {
+      mode: 1,
       currentPage: 1,
+      authCodeButtonText: '获取验证码',
+      authCodeButton: true,
       form: {
         name: '',
         gender: '',
@@ -123,19 +132,139 @@ export default {
         selfIntroduction: '',
         honor: ''
       },
-      authCode: ''
+      authCode: '',
+      AuthCode: '-1'
     }
   },
   methods: {
     turnPage () {
-      this.currentPage++
-      if (this.currentPage > 4) {
-        this.currentPage = 1
+      if (this.currentPage === 2) {
+        // 验证手机验证码
+        if (String(this.authCode) === String(this.AuthCode)) {
+          this.$message({
+            message: '验证成功',
+            type: 'success'
+          })
+        } else {
+          this.$message({
+            message: '验证码错误',
+            type: 'error'
+          })
+          return
+        }
       }
+      if (this.currentPage >= 4) {
+        // 提交表单
+        this.submitForm()
+        return
+      }
+      this.currentPage++
     },
     turnBack () {
       if (this.currentPage === 1) return
       this.currentPage--
+    },
+    getAuthCode () {
+      // 验证码获取方式替换为向服务器发送请求获取验证码
+      // 首先校验手机号是否正确
+      let message = this.$message
+      if (this.form.phone === '') {
+        message({
+          message: '请输入手机号',
+          type: 'error'
+        })
+        return
+      }
+      if (!this.form.phone.match(/^1[3-9][0-9]\d{8}$/)) {
+        message({
+          message: '请输入正确的手机号',
+          type: 'error'
+        })
+        return
+      }
+      // 生成四位数字验证码并存放在this.authCode中
+      if (this.AuthCode === '-1') {
+        this.AuthCode = String(Math.floor(Math.random() * 9000 + 1000))
+      }
+      let data = {
+        phone: this.form.phone,
+        timestamp: new Date().getTime(),
+        code: this.AuthCode
+      }
+      // 像/api/getAuthCode接口发送请求获取验证码，字段为mobile
+      let that = this
+      // 这里http://localhost:3000/api/getAuthCode 居然是发送到本地的3000端口，而不是发送到服务器的3000端口
+      // 发送到服务器的3000端口
+      // 判断开发模式还是生产模式
+      let url = 'http://localhost:3000/api/getAuthCode'
+      if (this.mode === 1) {
+        url = 'http://helloworld-hdu.com:3000/api/getAuthCode'
+      }
+      this.$axios.post(url, data).then(function (response, code) {
+        // console.log(response)
+        if (response.status === 200) {
+          // console.log('验证码发送成功')
+          that.AuthCode = response.data
+          // 禁用按钮60秒
+          let time = 60
+          let timer = setInterval(() => {
+            time--
+            if (time === 0) {
+              clearInterval(timer)
+              that.authCodeButton = true
+              that.authCodeButtonText = '获取验证码'
+            } else {
+              that.authCodeButton = false
+              that.authCodeButtonText = time + 's'
+            }
+          }, 1000)
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    // 表单提交
+    submitForm () {
+      let that = this
+      let message = this.$message
+      // 将表单提交到3000端口的/api/register接口
+      // http://localhost:3000/api/register 居然直接提交到了本地，而不是服务器的3000端口
+      // 判断开发模式还是生产模式
+      let url = 'http://localhost:3000/api/register'
+      if (this.mode === 1) {
+        url = 'http://helloworld-hdu.com:3000/api/register'
+      }
+      this.$axios.post(url, this.form).then(function (response, code) {
+        if (response.status === 200) {
+          message({
+            message: '提交成功',
+            type: 'success'
+          })
+          that.$router.push('/success')
+        }
+      }).catch((err) => {
+        // console.log(err)
+        // console.log(err.response)
+        let data = err.response.data
+        if (data.name === 'SequelizeUniqueConstraintError') {
+          if (data.msg.indexOf('stuNum') !== -1) {
+            message({
+              message: '学号已被注册',
+              type: 'error'
+            })
+          } else {
+            message({
+              message: '您已经提交过表单啦，请勿重复提交',
+              type: 'error'
+            })
+          }
+        } else {
+          message({
+            message: '提交失败',
+            type: 'error'
+          })
+        }
+      })
     }
   },
   watch: {
